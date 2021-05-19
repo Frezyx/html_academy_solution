dddd50df7c9b92fc869383be58017c4d9249d798
rodion-solovev-7 <rodion.solovev.7@ya.ru>
2021-05-20 03:57:10 +0500

    refactoring

--- solver.py
+++ solver.py
@@ -1,4 +1,4 @@
-import getpass
+from getpass import getpass
 from os import environ
 
 import selenium.webdriver.support.expected_conditions as EC
@@ -7,18 +7,6 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-
-login = environ.get('LOGIN', None)
-password = environ.get('PASSWORD', None)
-
-if login is None:
-    login = input("Введите логин HTML Academy: ")
-if password is None:
-    password = getpass.getpass("Введите пароль HTML Academy: ")
-
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Firefox()
-driver.get("https://htmlacademy.ru/login")
 
 
 def set_text(input_form_id, value):
@@ -33,7 +21,6 @@ def set_text(input_form_id, value):
 def sign_in():
     global driver
 
-    print('Логинюсь')
     set_text("login-email", login)
     set_text("login-password", password)
     submit = driver.find_element_by_css_selector(".button--full-width[type='submit']")
@@ -49,79 +36,74 @@ def get_tasks_count():
     return int(parts[1])
 
 
-def solve_task():
+def solve_task(task_url):
     global driver
+
+    driver.get(task_url)
 
     waiter15 = WebDriverWait(driver, 15)
     waiter40 = WebDriverWait(driver, 40)
 
-    locator = ('UNDEFINED', 'UNDEFINED')
+    locator = (By.CSS_SELECTOR, ".course-theory__close.icon-close")
+    close = waiter15.until(EC.visibility_of_element_located(locator))
+    close.click()
 
-    try:
-        locator = (By.CSS_SELECTOR, ".course-theory__close.icon-close")
-        close = waiter15.until(EC.visibility_of_element_located(locator))
-        close.click()
+    locator = (By.CSS_SELECTOR, ".course-editor-controls__item--answer")
+    show_answer = waiter15.until(EC.visibility_of_element_located(locator))
+    # warning: костыль против "element is not clickable because another element obscures it"
+    driver.execute_script("arguments[0].click();", show_answer)
 
-        locator = (By.CSS_SELECTOR, ".course-editor-controls__item--answer")
-        show_answer = waiter15.until(EC.visibility_of_element_located(locator))
-        # warning: костыль против "element is not clickable because another element obscures it"
-        driver.execute_script("arguments[0].click();", show_answer)
-
-        waiter40.until(EC.text_to_be_present_in_element(locator, "Показать ответ"))
-    except TimeoutException:
-        print(f"Время ожидания элемента '{locator[1]}' вышло")
-    except Exception as e:
-        print(f"Произошла ошибка при решении: {e}")
+    waiter40.until(EC.text_to_be_present_in_element(locator, "Показать ответ"))
 
 
-def run_solve(count, trainer_url):
+def solve_trainer_tasks(count, trainer_url):
     global driver
 
     for i in range(1, count + 1):
         now_url = f"{trainer_url}/{i}"
         print(now_url)
-        driver.get(now_url)
-        solve_task()
+        try:
+            solve_task(now_url)
+        except TimeoutException:
+            print("Время ожидания элемента вышло")
+        except Exception as e:
+            print(f"Произошла ошибка при решении: {e}")
 
 
-def get_trainer_links():
+def get_trainer_links_id():
     global driver
 
-    print('Собираю ссылки на все тренажёры...')
-    driver.get('https://htmlacademy.ru/courses')
+    print("Собираю ссылки на все тренажёры...")
+    driver.get("https://htmlacademy.ru/courses")
+
+    courses_links = (a.get_attribute("href") for a in driver.find_elements_by_tag_name("a"))
+    courses_links = (course_link for course_link in courses_links if course_link.find("courses/") != -1)
+    courses_links = set(courses_links)
 
     trainers_links = []
-    all_links = driver.find_elements_by_tag_name('a')
-    courses_links = []
+    for course_link in courses_links:
+        driver.get(course_link)
 
-    for link in all_links:
-        courses_links.append(link.get_attribute('href'))
+        page_links = (a.get_attribute("href") for a in driver.find_elements_by_tag_name("a"))
+        page_links = (page_link for page_link in page_links if page_link.find("continue/course/") != -1)
 
-    courses_links = list(set(courses_links))
+        trainers_links.extend(page_links)
 
-    for course_href in courses_links:
-        if course_href.find('courses/') == -1:
-            continue
+    links_id = (int(link[link.rfind('/') + 1:]) for link in trainers_links)
 
-        driver.get(course_href)
-        course_page_links = driver.find_elements_by_tag_name('a')
-
-        for page_link in course_page_links:
-            href_page_link = page_link.get_attribute('href')
-            if href_page_link.find('continue/course/') != -1:
-                trainers_links.append(href_page_link)
-
-    print('Будто бы всё собрал...')
-    return list(set(trainers_links))
+    print("Будто бы всё собрал...")
+    return sorted(set(links_id))
 
 
 def solve():
     global driver
 
+    print("Логинюсь")
     sign_in()
-    print('Давай короче я погнал')
 
-    trainer_link = 'https://htmlacademy.ru/continue/course'
+    print("Давай короче я погнал")
+
+    trainer_link = "https://htmlacademy.ru/continue/course"
 
     links_id = [
         39, 42, 44, 45, 46, 50, 51, 53, 55, 57, 58, 65, 66, 70, 71, 73, 74, 76, 79, 80, 84, 85, 86, 88, 96, 97, 98,
@@ -131,15 +113,26 @@ def solve():
     ]
 
     for link_id in links_id:
-        url = f'{trainer_link}/{link_id}'
-        driver.get(url)
-
-        url = driver.current_url
+        driver.get(f'{trainer_link}/{link_id}')
         count_tasks = get_tasks_count()
 
+        url = driver.current_url
         trainer_url = url[:url.rfind('/')]
-        run_solve(count_tasks, trainer_url)
+
+        solve_trainer_tasks(count_tasks, trainer_url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    login = environ.get("LOGIN", None)
+    password = environ.get("PASSWORD", None)
+
+    if login is None:
+        login = input("Введите логин HTML Academy: ")
+    if password is None:
+        password = getpass("Введите пароль HTML Academy: ")
+
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Firefox()
+    driver.get("https://htmlacademy.ru/login")
+
     solve()
