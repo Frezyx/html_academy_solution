181aea0b44837046ddfd293c489ca0ada683c89a
rodion-solovev-7 <rodion.solovev.7@ya.ru>
2021-05-20 14:43:48 +0500

    Many fixes for multiprocessing, exceptions with description and other

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ from os import environ
 
 import selenium.webdriver.support.expected_conditions as EC
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -13,7 +13,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 # global
-unsolved_tasks_urls = []
 login = None
 password = None
 
@@ -41,10 +40,10 @@ def init_driver():
 
 
 def set_text(driver, input_form_id, value):
-    email_elem = driver.find_element_by_id(input_form_id)
-    email_elem.click()
-    email_elem.clear()
-    email_elem.send_keys(value)
+    input_elem = driver.find_element_by_id(input_form_id)
+    input_elem.click()
+    input_elem.clear()
+    input_elem.send_keys(value)
 
 
 def sign_in(driver, login, password):
@@ -58,7 +57,14 @@ def sign_in(driver, login, password):
 def get_tasks_count(driver, trainer_url):
     driver.get(trainer_url)
 
-    count_elem = driver.find_element_by_css_selector(".course-nav__stat")
+    waiter10 = WebDriverWait(driver, 10)
+    locator = (By.CSS_SELECTOR, ".course-nav__stat")
+
+    try:
+        count_elem = waiter10.until(EC.visibility_of_element_located(locator))
+    except TimeoutException:
+        raise Exception(f"Невозможно определить кол-во заданий")
+
     parts = count_elem.text.split('/')
     return int(parts[1])
 
@@ -66,33 +72,30 @@ def get_tasks_count(driver, trainer_url):
 def solve_task(driver, task_url):
     driver.get(task_url)
 
+    if any(driver.find_elements_by_css_selector(".course-challenge-controls__button")):
+        raise Exception("Испытания не поддерживаются")
+
+    if not any(driver.find_elements_by_css_selector(".course-layout__sidebar")):
+        raise Exception("Конспекты не поддерживаются")
+
     waiter15 = WebDriverWait(driver, 15)
     waiter40 = WebDriverWait(driver, 40)
 
-    locator = (By.CSS_SELECTOR, ".course-theory__close.icon-close")
-    close = waiter15.until(EC.visibility_of_element_located(locator))
-    close.click()
+    locator = ("UNDEFINED", "UNDEFINED")
 
-    locator = (By.CSS_SELECTOR, ".course-editor-controls__item--answer")
-    show_answer = waiter15.until(EC.visibility_of_element_located(locator))
-    # warning: костыль против "element is not clickable because another element obscures it"
-    driver.execute_script("arguments[0].click();", show_answer)
+    try:
+        locator = (By.CSS_SELECTOR, ".course-theory__close.icon-close")
+        close = waiter15.until(EC.visibility_of_element_located(locator))
+        close.click()
 
-    waiter40.until(EC.text_to_be_present_in_element(locator, "Показать ответ"))
+        locator = (By.CSS_SELECTOR, ".course-editor-controls__item--answer")
+        show_answer = waiter15.until(EC.visibility_of_element_located(locator))
+        # warning: костыль против "element is not clickable because another element obscures it"
+        driver.execute_script("arguments[0].click();", show_answer)
 
-
-def solve_tasks(driver, count, trainer_url):
-    for i in range(1, count + 1):
-        task_url = f"{trainer_url}/{i}"
-
-        try:
-            solve_task(driver, task_url)
-        except TimeoutException:
-            print(f"Время ожидания элемента вышло ({task_url})")
-            unsolved_tasks_urls.append(task_url)
-        except Exception as e:
-            print(f"Произошла ошибка при решении ({task_url}): {e}")
-            unsolved_tasks_urls.append(task_url)
+        waiter40.until(EC.text_to_be_present_in_element(locator, "Показать ответ"))
+    except TimeoutException:
+        raise Exception(f"Время ожидания элемента '{locator[1]}' вышло")
 
 
 def get_trainer_links_id(driver):
@@ -118,31 +121,44 @@ def get_trainer_links_id(driver):
     return sorted(set(links_id))
 
 
-def solve(driver, links_id):
-    print("Давай короче я погнал")
-
-    trainer_template = "https://htmlacademy.ru/continue/course"
-
-    for link_id in links_id:
-        trainer_url = f'{trainer_template}/{link_id}'
+def collect_task_urls_from_trainers(driver, trainer_urls):
+    for trainer_url in trainer_urls:
         try:
-            count_tasks = get_tasks_count(driver, trainer_url)
-        except Exception:
-            print(f"Невозможно найти кол-во заданий ({trainer_url})")
+            tasks_count = get_tasks_count(driver, trainer_url)
+        except Exception as e:
+            print(f"Произошла ошибка при поиске кол-ва заданий ({trainer_url}): {e}")
             continue
 
         trainer_url = driver.current_url
         trainer_url = trainer_url[:trainer_url.rfind('/')]
 
-        solve_tasks(driver, count_tasks, trainer_url)
+        yield from (f"{trainer_url}/{i}" for i in range(1, tasks_count + 1))
 
 
-def process_task(links_id):
+def solve_tasks_by_urls(driver, task_urls):
+    for task_url in task_urls:
+        try:
+            solve_task(driver, task_url)
+        except Exception as e:
+            print(f"Произошла ошибка при решении ({task_url}): {e}")
+            # unsolved_tasks_urls.append(task_url)
+
+
+def ptask_collect_tasks(links_id):
     with init_driver() as driver:
-        print("Логинюсь")
+        sign_in(driver, login, password)
+        return list(collect_task_urls_from_trainers(driver, links_id))
+
+
+def ptask_solve_tasks(task_urls):
+    with init_driver() as driver:
         sign_in(driver, login, password)
 
-        solve(driver, links_id)
+        solve_tasks_by_urls(driver, task_urls)
+
+
+def split_tasks(tasks, workers_count):
+    return (tasks[i::workers_count] for i in range(workers_count))
 
 
 def main():
@@ -156,20 +172,34 @@ def main():
         password = getpass("Введите пароль HTML Academy: ")
 
     links_id = [
-        39, 42, 44, 45, 46, 50, 51, 53, 55, 57, 58, 65, 66, 70, 71, 73, 74, 76, 79, 80, 84, 85, 86, 88, 96, 97, 98,
-        102, 103, 104, 113, 125, 128, 129, 130, 156, 157, 158, 165, 187, 195, 197, 199, 207, 209, 211, 213, 215, 217,
-        219, 259, 269, 273, 297, 299, 301, 303, 305, 307, 309, 337, 339, 341, 343, 345, 347, 349, 351, 353, 355, 357,
-        359, 365, 367
+        39, 42, 44, 45, 46, 50, 51, 53, 55, 57, 58, 65, 66, 70, 71, 73, 74, 76, 79, 80,
+        84, 85, 86, 88, 96, 97, 98, 102, 103, 104, 113, 125, 128, 129, 130, 156, 157, 158,
+        # 165,
+        187, 195, 197, 199, 207, 209, 211, 213, 215, 217, 219, 259, 269, 273, 297, 299,
+        301, 303, 305, 307, 309, 337, 339, 341, 343,
+        # 345,
+        347, 349, 351, 353, 355, 357, 359, 365, 367,
     ]
 
-    process_count = cpu_count()
-    chunk_size = len(links_id) // process_count + len(links_id) % process_count
-    chunked_links_id = (links_id[chunk_size * i: chunk_size * (i + 1)] for i in range(process_count))
+    links = [f'{"https://htmlacademy.ru/continue/course"}/{link_id}' for link_id in links_id]
+
+    # process_count = cpu_count()
+    process_count = 4
 
     with Pool(process_count) as pool:
-        pool.map(process_task, chunked_links_id)
+        chunked_links = split_tasks(links, process_count)
+        results = pool.map(ptask_collect_tasks, chunked_links)
 
-    print("\nНерешённые задания:", *unsolved_tasks_urls, sep='\n')
+        task_urls = []
+        for result in results:
+            task_urls.extend(result)
+
+        print("Ссылки собраны")
+
+        chunked_task_urls = split_tasks(task_urls, process_count)
+        pool.map(ptask_solve_tasks, chunked_task_urls)
+
+    # print("\nНерешённые задания:", *unsolved_tasks_urls, sep='\n')
 
 
 if __name__ == "__main__":
